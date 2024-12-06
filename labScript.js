3ff00587d63f8d7254c35a69cc30f66b2fa3943d
jaypog-web <jay.kim@uleth.ca>
2024-12-06 07:26:10 -0700

    Added cloning function, risky states function

--- labScript.js
+++ labScript.js
@@ -19,3 +19,21 @@ $('.viewButton').on('click', function () {
 
     changeView(this, currentDisplay);
 });
+
+//This function is used to switch the character and the apparatus to their risky/warning states when an element is low
+function riskyState() {
+    $('.himView').css('background-image', 'url("../images/vatview_risk.gif")');
+    $('.apparatusView').css('background-image', 'url("../images/apparatusView_risk.gif")');
+}
+
+//Change clone chamber upon clicking clone button. Also remove the option/suggestion to clone again, and call the riskyState function
+$('.cloneButton').on('click', function () {
+    $('.cloneChamber').css('background-image', 'url("../images/clone-develop.gif")');
+    $('.cloneUpdate').html('<h2>CLONE STATUS:</h2> <h3>DEVELOPING...</h3>');
+    $('.cloneButton').addClass('cloneButtonPressed');
+    $('.cloneButton').removeClass('cloneButton');
+
+    riskyState();
+
+    $(this).unbind('click');
+});
